496db3eee3c0c4706136aab78938ef559ba6f40f
yanSilva <yansiba84@gmail.com>
2020-11-05 23:27:41 -0300

    Migrando o site para express-generator e restify

---
+++ client-server/routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var assert = require('assert');
+var router = express.Router();
+var restify = require('restify-clients');
+
+// Creates a JSON client
+var client = restify.createJsonClient({
+  url: 'http://localhost:4000'
+});
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  client.get('/users', function(err, request, response, obj) {
+    assert.ifError(err);
+    
+    res.end(JSON.stringify(obj, null, 2));
+  });
+});
+
+module.exports = router;
